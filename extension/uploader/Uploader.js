57019074af7d03b7ff27bfc8ba587cc90a124f10
Aldwin Su <fangyansu@163.com>
2019-03-22 17:25:38 +0800

    Add Uploader.js

---
+++ extension/uploader/Uploader.js
@@ -0,0 +1,56 @@
+/**
+ * Uploader.js v20190322
+ * dependency: jQuery.js, devutility.js
+ * @license: MIT (c) Aldwin Su. https://github.com/eagle6688
+ */
+
+(function ($, window, document, undefined) {
+    var pluginName = 'Uploader';
+
+    var defaults = {};
+
+    function Plugin(options) {
+        this.options = $.extend({}, defaults, options);
+        this._init();
+    }
+
+    Plugin.prototype.constructor = Plugin;
+
+    /* Init methods */
+
+    Plugin.prototype._init = function () {
+        if (!this._verify()) {
+            return;
+        }
+
+        this._bind();
+    };
+
+    Plugin.prototype._verify = function () {
+        return true;
+    };
+
+    /* Init methods end */
+
+    /* Bind methods */
+
+    Plugin.prototype._bind = function () {
+
+    };
+
+    /* Bind methods end */
+
+    /* Event methods */
+
+    /* Event methods end */
+
+    /* Methods internal methods */
+
+    /* Methods end */
+
+    /* Public methods */
+
+    /* Public methods end */
+
+    window[pluginName] = Plugin;
+})(jQuery, window, document);
