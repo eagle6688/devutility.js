79345ea36c3d4bfcd828f96d11a419a9191e9639
Su Liang <fangyansu@163.com>
2019-03-24 22:45:42 +0800

    Update

--- extension/uploader/Uploader.js
+++ extension/uploader/Uploader.js
@@ -9,11 +9,22 @@
 
     var defaults = {
         url: '', //Post url for uploading file.
-        files: [], //Files for uploading, note that all files should be in a same queue.
         concurrency: 1, //Concurrency thread for uploading.
         isSliced: true, //Whether slice a file to multi pieces or not?
         pieceSize: 1 * 1024 * 1024, //Bytes for each piece, worked if "isSliced"=true.
-        checksum: function (callback) {} //
+        retry: 1, //Retry times after upload failed.
+        checksum: function (callback) {} //Function to calculate file's checksum.
+    };
+
+    var config = {
+        data: {
+            name: '', //File or file piece name.
+            size: 0, //Actual size or file or file piece.
+            index: 0, //Index of file piece, 0 for file.
+            count: 1, //Amount of file pieces, 1 for whole file.
+            checksum: null, //Checksum of file or file piece.
+            timestamp: 0 //Timestamp for uploading.
+        }
     };
 
     function Plugin(options) {
@@ -33,7 +44,39 @@
         this._bind();
     };
 
+    Plugin.prototype._initQueue = function () {
+        this.queue = [];
+        this.failedQueue = [];
+
+
+    };
+
     Plugin.prototype._verify = function () {
+        if (!window.FormData) {
+            console.error('Your browser does not support this function!');
+            return false;
+        }
+
+        if (!this.options.url) {
+            console.error('Need paramter "url"!');
+            return false;
+        }
+
+        if (!this.options.files || this.options.files.length == 0) {
+            console.error('Need paramter "files"!');
+            return false;
+        }
+
+        if (!this.options.concurrency < 1) {
+            console.error('Invalid paramter "concurrency"!');
+            return false;
+        }
+
+        if (!this.options.retry < 0) {
+            console.error('Invalid paramter "retry"!');
+            return false;
+        }
+
         return true;
     };
 
@@ -56,6 +99,10 @@
     /* Methods end */
 
     /* Public methods */
+
+    Plugin.prototype.upload = function (files) {
+
+    };
 
     /* Public methods end */
 
