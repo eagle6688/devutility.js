070cab2a7234f33cdf55afac4d0fca276e71e8e4
Aldwin Su <fangyansu@163.com>
2019-03-26 19:44:14 +0800

    Update Uploader.js

--- extension/uploader/Uploader.js
+++ extension/uploader/Uploader.js
@@ -10,21 +10,11 @@
     var defaults = {
         url: '', //Post url for uploading file.
         concurrency: 1, //Concurrency thread for uploading.
-        isSliced: true, //Whether slice a file to multi pieces or not?
-        pieceSize: 1 * 1024 * 1024, //Bytes for each piece, worked if "isSliced"=true.
+        needSlice: false, //Whether slice a file to multi pieces or not?
+        pieceSize: 1 * 1024 * 1024, //Bytes for each piece, worked if "needSlice"=true.
         retry: 1, //Retry times after upload failed.
+        formData: null, //FormData for each upload request.
         checksum: function (callback) {} //Function to calculate file's checksum.
-    };
-
-    var config = {
-        data: {
-            name: '', //File or file piece name.
-            size: 0, //Actual size or file or file piece.
-            index: 0, //Index of file piece, 0 for file.
-            count: 1, //Amount of file pieces, 1 for whole file.
-            checksum: null, //Checksum of file or file piece.
-            timestamp: 0 //Timestamp for uploading.
-        }
     };
 
     function Plugin(options) {
@@ -41,14 +31,9 @@
             return;
         }
 
-        this._bind();
-    };
-
-    Plugin.prototype._initQueue = function () {
+        this.total = 0;
         this.queue = [];
         this.failedQueue = [];
-
-
     };
 
     Plugin.prototype._verify = function () {
@@ -67,12 +52,12 @@
             return false;
         }
 
-        if (!this.options.concurrency < 1) {
+        if (this.options.concurrency < 1) {
             console.error('Invalid paramter "concurrency"!');
             return false;
         }
 
-        if (!this.options.retry < 0) {
+        if (this.options.retry < 0) {
             console.error('Invalid paramter "retry"!');
             return false;
         }
@@ -82,19 +67,59 @@
 
     /* Init methods end */
 
-    /* Bind methods */
-
-    Plugin.prototype._bind = function () {
-
-    };
-
-    /* Bind methods end */
-
     /* Event methods */
 
     /* Event methods end */
 
-    /* Methods internal methods */
+    /* Methods */
+
+    function UploaderData() {
+        this.name = ''; //File or file piece name.
+        this.size = 0; //Actual size or file or file piece.
+        this.index = 0; //Index of file piece, 0 for file.
+        this.count = 1; //Amount of file pieces, 1 for whole file.
+        this.checksum = null; //Checksum of file or file piece.
+        this.timestamp = 0; //Timestamp for uploading.
+        this.properties = Object.keys(this);
+    }
+
+    UploaderData.prototype.toFormData = function () {
+        var formData = this.options.formData;
+
+        if (!formData) {
+            formData = new FormData();
+        }
+
+        for (var name in this.properties) {
+            formData.set(name, this[name]);
+        }
+
+        return formData;
+    };
+
+    Plugin.prototype._enqueue = function (file) {
+        if (!this.options.needSlice) {
+            this.total = this.queue.push(file);
+            return;
+        }
+
+        var pieceCount = Math.ceil(file.size / this.options.pieceSize);
+
+        for (var i = 0; i < pieceCount; i++) {
+            var start = i * this.options.pieceSize,
+                end = Math.min(file.size, start + this.options.pieceSize);
+
+            var piece = this._createSlice(i, start, end);
+        }
+    };
+
+    Plugin.prototype._createUploaderData = function (file) {
+
+    };
+
+    Plugin.prototype._percentage = function () {
+        return Math.floor(100 * (this.total - this.queue.length) / this.total);
+    };
 
     /* Methods end */
 
