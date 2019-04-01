e690830ef1571cf94e7955dce5d9b487f7e13315
Su Liang <fangyansu@163.com>
2019-04-01 23:12:49 +0800

    Add upload event operation for HttpRequest.js

--- extension/http-request/HttpRequest.js
+++ extension/http-request/HttpRequest.js
@@ -17,11 +17,17 @@
         progress: function (data) {},
         complete: function (data) {},
         failed: function (data) {},
-        abort: function (data) {}
+        abort: function (data) {},
+        upload: {
+            progress: function (data) {},
+            complete: function (data) {},
+            failed: function (data) {},
+            abort: function (data) {}
+        }
     };
 
     function Plugin(options) {
-        this.options = $.extend({}, defaults, options);
+        this.options = $.extend(true, {}, defaults, options);
         this._init();
     }
 
@@ -55,76 +61,28 @@
     Plugin.prototype._bind = function () {
         var self = this;
 
-        this.xhr.addEventListener('progress', function (event) {
-            self._progress(event);
+        this.requestEvent = new RequestEvent({
+            request: this.xhr,
+            dataType: this.options.dataType,
+            progress: this.options.progress,
+            complete: this.options.complete,
+            failed: this.options.failed,
+            abort: this.options.abort
         });
 
-        this.xhr.addEventListener('load', function (event) {
-            self._complete(event);
-        });
-
-        this.xhr.addEventListener('error', function (event) {
-            self._failed(event);
-        });
-
-        this.xhr.addEventListener('abort', function (event) {
-            self._abort(event);
+        this.uploadRequestEvent = new RequestEvent({
+            request: this.xhr.upload,
+            dataType: this.options.dataType,
+            progress: this.options.upload.progress,
+            complete: this.options.upload.complete,
+            failed: this.options.upload.failed,
+            abort: this.options.upload.abort
         });
     };
 
     /* Bind methods end */
 
-    /* Event methods */
-
-    Plugin.prototype._progress = function (event) {
-        var result = this._result(event);
-
-        if (this.options.progress) {
-            this.options.progress(result);
-        }
-    };
-
-    Plugin.prototype._complete = function (event) {
-        var result = this._result(event);
-
-        if (this.options.complete) {
-            this.options.complete(result);
-        }
-    };
-
-    Plugin.prototype._failed = function (event) {
-        var result = this._result(event);
-
-        if (this.options.failed) {
-            this.options.failed(result);
-        }
-    };
-
-    Plugin.prototype._abort = function (event) {
-        var result = this._result(event);
-
-        if (this.options.abort) {
-            this.options.abort(result);
-        }
-    };
-
-    /* Event methods end */
-
     /* Methods */
-
-    Plugin.prototype._result = function (event) {
-        var result = {
-            event: event,
-            status: this.xhr.status,
-            readyState: this.xhr.readyState
-        };
-
-        if (this.options.dataType == 'json') {
-            result.response = $.parseJSON(this.xhr.responseText);
-        }
-
-        return result;
-    };
 
     Plugin.prototype._setXhr = function () {
         if (this.options.timeout > 0) {
@@ -143,6 +101,101 @@
     };
 
     /* Methods end */
+
+    /* RequestEvent */
+
+    function RequestEvent(options) {
+        var defaults = {
+            request: null,
+            dataType: null,
+            progress: function (data) {},
+            complete: function (data) {},
+            failed: function (data) {},
+            abort: function (data) {}
+        };
+
+        this.options = $.extend(true, {}, defaults, options);
+        this.bind();
+    }
+
+    RequestEvent.prototype.bind = function () {
+        var self = this;
+
+        this.options.request.addEventListener('progress', function (event) {
+            self.progress(event);
+        });
+
+        this.options.request.addEventListener('load', function (event) {
+            self.complete(event);
+        });
+
+        this.options.request.addEventListener('error', function (event) {
+            self.failed(event);
+        });
+
+        this.options.request.addEventListener('abort', function (event) {
+            self.abort(event);
+        });
+    };
+
+    RequestEvent.prototype.progress = function (event) {
+        var result = this.result(event);
+
+        if (this.options.progress) {
+            this.options.progress(result);
+        }
+    };
+
+    RequestEvent.prototype.complete = function (event) {
+        var result = this.result(event);
+
+        if (this.options.complete) {
+            this.options.complete(result);
+        }
+    };
+
+    RequestEvent.prototype.failed = function (event) {
+        var result = this.result(event);
+
+        if (this.options.failed) {
+            this.options.failed(result);
+        }
+    };
+
+    RequestEvent.prototype.abort = function (event) {
+        var result = this.result(event);
+
+        if (this.options.abort) {
+            this.options.abort(result);
+        }
+    };
+
+    RequestEvent.prototype.result = function (event) {
+        var result = {
+            type: event.type,
+            event: event,
+            status: 0,
+            readyState: 0
+        };
+
+        if (Object.prototype.toString.call(event.target) == '[object XMLHttpRequestUpload]') {
+            result.type = 'upload-' + result.type;
+            return result;
+        }
+
+        result.status = event.target.status;
+        result.readyState = event.target.readyState;
+
+        if (this.options.dataType == 'json') {
+            result.response = $.parseJSON(event.target.responseText);
+        } else {
+            result.response = event.target.responseText;
+        }
+
+        return result;
+    };
+
+    /* RequestEvent end */
 
     /* Public methods */
 
